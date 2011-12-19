c93b713273e9a1f12d25fe74f940778e25398284
Alex Suraci <suraci.alex@gmail.com>
2011-12-19 15:35:19 -0800

    send Content-Length header
    Change-Id: I9beed314633003c268f42032af46563ff4e48091

--- lib/caldecott/client/http_tunnel.rb
+++ lib/caldecott/client/http_tunnel.rb
@@ -75,7 +75,7 @@ module Caldecott
           parsed_uri.path = '/tunnels'
 
           @log.debug "post #{parsed_uri.to_s}"
-          req = EM::HttpRequest.new(parsed_uri.to_s).post :body => init_msg, :head => { "Auth-Token" => @auth_token }
+          req = EM::HttpRequest.new(parsed_uri.to_s).post :body => init_msg, :head => { "Auth-Token" => @auth_token, "Content-Length" => init_msg.bytesize }
 
           req.callback do
             @log.debug "post #{parsed_uri.to_s} #{req.response_header.status}"
@@ -210,7 +210,7 @@ module Caldecott
 
           uri = "#{@uri}/#{@seq}"
           @log.debug "put #{uri}"
-          req = EM::HttpRequest.new(uri).put :body => data, :head => { "Auth-Token" => @auth_token }
+          req = EM::HttpRequest.new(uri).put :body => data, :head => { "Auth-Token" => @auth_token, "Content-Length" => data.bytesize }
 
           req.errback do
             @log.debug "put #{uri} error"

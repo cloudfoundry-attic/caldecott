579a4462eee928f75d276fbad3448ad21aa3f7c4
Maria Shaldybina <mariash@vmware.com>
2012-11-21 20:14:32 -0800

    Remove eventmachine from client
    TODO: add websockets support
    
    Change-Id: I2da021d696026fc437b8d3b9bf85235c02c9cb28

--- client/lib/caldecott-client/tunnel/http_tunnel.rb
+++ client/lib/caldecott-client/tunnel/http_tunnel.rb
@@ -1,240 +1,132 @@
 # Copyright (c) 2009-2011 VMware, Inc.
 
-require 'em-http'
-require 'json'
+require "uri"
+require "net/http"
+require "json"
 
 module Caldecott
   module Client
-    class HttpTunnel
+    class HttpTunnel < Tunnel
       MAX_RETRIES = 10
 
-      def initialize(logger, url, dst_host, dst_port, auth_token)
-        @log, @auth_token = logger, auth_token
-        @closing = false
-        init_msg = ""
+      attr_reader :path_in, :path_out, :write_seq, :read_seq
 
-        # FIXME: why is this optional?
-        if dst_host
-          init_msg = { :host => dst_host, :port => dst_port }.to_json
-        end
-
-        start(url, init_msg)
-      end
-
-      def onopen(&blk)
-        @onopen = blk
-        @onopen.call if @opened
-      end
-
-      def onclose(&blk)
-        @onclose = blk
-        @onclose.call if @closed
-      end
-
-      def onreceive(&blk)
-        @onreceive = blk
-      end
-
-      def send_data(data)
-        @writer.send_data(data)
-      end
-
-      def close
-        return if @closing or @closed
-        @closing = true
-        @writer.close if @writer
-        @reader.close if @reader
-        stop
-      end
-
-      def trigger_on_open
-        @opened = true
-        @onopen.call if @onopen
-      end
-
-      def trigger_on_close
-        close
-        @closed = true
-        @onclose.call if @onclose
-        @onclose = nil
-      end
-
-      def trigger_on_receive(data)
-        @onreceive.call(data)
-      end
-
-      def start(base_uri, init_msg, attempts = MAX_RETRIES)
-        if attempts <= 0
-          trigger_on_close
-          return
-        end
+      def initialize(url, options)
+        super
 
         begin
-          parsed_uri = Addressable::URI.parse(base_uri)
-          parsed_uri.path = '/tunnels'
-
-          @log.debug "post #{parsed_uri.to_s}"
-          req = EM::HttpRequest.new(parsed_uri.to_s).post :body => init_msg, :head => { "Auth-Token" => @auth_token, "Content-Length" => init_msg.bytesize }
-
-          req.callback do
-            @log.debug "post #{parsed_uri.to_s} #{req.response_header.status}"
-            unless [200, 201, 204].include?(req.response_header.status)
-              start(base_uri, init_msg, attempts - 1)
-            else
-              resp = JSON.parse(req.response)
-
-              parsed_uri.path = resp["path"]
-              @tun_uri = parsed_uri.to_s
-
-              parsed_uri.path = resp["path_out"]
-              @reader = Reader.new(@log, parsed_uri.to_s, self, @auth_token)
-
-              parsed_uri.path = resp["path_in"]
-              @writer = Writer.new(@log, parsed_uri.to_s, self, @auth_token)
-              trigger_on_open
-            end
-          end
-
-          req.errback do
-            @log.debug "post #{parsed_uri.to_s} error"
-            start(base_uri, init_msg, attempts - 1)
-          end
-
-        rescue Exception => e
-          @log.error e
-          trigger_on_close
-          raise e
+          @tun_url = URI.parse(url)
+        rescue URI::Error
+          raise ClientError, "Parsing tunnel URL failed"
         end
+
+        @path_in = nil
+        @path_out = nil
+        @write_seq = nil
+        @read_seq = nil
       end
 
-      def stop(attempts = MAX_RETRIES)
-        if attempts <= 0
-          trigger_on_close
+      def start
+        req = Net::HTTP::Post.new("/tunnels")
+        req.body = JSON.generate(:host => dst_host, :port => dst_port)
+        req["Content-Type"] = "application/json"
+
+        resp = request(req, "creating tunnel remotely", [200, 201, 204])
+
+        begin
+          payload = JSON.parse(resp.body)
+        rescue
+          raise ClientError, "Parsing response data failed"
+        end
+
+        @tunnel_path = payload["path"]
+        @path_in = payload["path_in"]
+        @path_out = payload["path_out"]
+
+        logger.info("Init success: tunnel_path=#{@tunnel_path}, " +
+                        "path_in=#{@path_in}, path_out=#{@path_out}")
+        @read_seq = 1
+        @write_seq = 1
+      end
+
+      def write(data)
+        if @path_in.nil? || @write_seq.nil?
+          raise ClientError, "Cannot write, tunnel isn't ready"
+        end
+
+        req = Net::HTTP::Put.new(@path_in + "/#{@write_seq}")
+        req.body = data
+        logger.debug("Sending #{data.bytesize} bytes")
+
+        resp = request(req, "sending data", [200, 202, 204, 404, 410])
+
+        if [404, 410].include?(resp.code.to_i)
+          close
           return
         end
 
-        return if @tun_uri.nil?
-
-        @log.debug "delete #{@tun_uri}"
-        req = EM::HttpRequest.new("#{@tun_uri}").delete :head => { "Auth-Token" => @auth_token }
-
-        req.errback do
-          @log.debug "delete #{@tun_uri} error"
-          stop(attempts - 1)
-        end
-
-        req.callback do
-          @log.debug "delete #{@tun_uri} #{req.response_header.status}"
-          if [200, 202, 204, 404].include?(req.response_header.status)
-            trigger_on_close
-          else
-            stop(attempts - 1)
-          end
-        end
+        @write_seq += 1
       end
 
-      class Reader
-        def initialize(log, uri, conn, auth_token)
-          @log, @base_uri, @conn, @auth_token = log, uri, conn, auth_token
-          @closing = false
-          start
+      def read
+        if @path_out.nil? || @read_seq.nil?
+          raise ClientError, "Cannot read, tunnel isn't ready"
         end
 
-        def close
-          @closing = true
+        req = Net::HTTP::Get.new(@path_out + "/#{@read_seq}")
+        resp = request(req, "reading data", [200, 404, 410])
+
+        @read_seq += 1
+
+        if [404, 410].include?(resp.code.to_i)
+          close
+          return ""
         end
 
-        def start(seq = 1, attempts = MAX_RETRIES)
-          return if @closing
-
-          if attempts <= 0
-            @conn.trigger_on_close
-            return
-          end
-
-          uri = "#{@base_uri}/#{seq}"
-          @log.debug "get #{uri}"
-          req = EM::HttpRequest.new(uri).get :timeout => 0, :head => { "Auth-Token" => @auth_token }
-
-          req.errback do
-            @log.debug "get #{uri} error"
-            start(seq, attempts - 1)
-          end
-
-          req.callback do
-            @log.debug "get #{uri} #{req.response_header.status}"
-            case req.response_header.status
-            when 200
-              @conn.trigger_on_receive(req.response)
-              start(seq + 1)
-            when 404
-              @conn.trigger_on_close
-            else
-              start(seq, attempts - 1)
-            end
-          end
-        end
+        resp.body
+      rescue EOFError # HTTP quirk?
+        ""
       end
 
-      class Writer
-        def initialize(log, uri, conn, auth_token)
-          @log, @uri, @conn, @auth_token = log, uri, conn, auth_token
-          @seq, @write_buffer = 1, ""
-          @closing = @writing = false
-        end
-
-        def send_data(data)
-          @write_buffer << data
-          send_data_buffered unless @writing
-        end
-
-        def close
-          @closing = true
-        end
-
-        private
-
-        def send_data_buffered(attempts = MAX_RETRIES)
-          return if @closing
-
-          @writing = true
-
-          @log.debug "attempts left: #{attempts}"
-          if attempts <= 0
-            @conn.trigger_on_close
-            return
-          end
-
-          data, @write_buffer = @write_buffer, ""
-
-          uri = "#{@uri}/#{@seq}"
-          @log.debug "put #{uri}"
-          req = EM::HttpRequest.new(uri).put :body => data, :head => { "Auth-Token" => @auth_token, "Content-Length" => data.bytesize }
-
-          req.errback do
-            @log.debug "put #{uri} error"
-            send_data_buffered(attempts - 1)
-          end
-
-          req.callback do
-            @log.debug "put #{uri} #{req.response_header.status}"
-            case req.response_header.status
-            when 200, 202, 204
-              @seq += 1
-
-              if @write_buffer.empty?
-                @writing = false
-              else
-                send_data_buffered
-              end
-            when 404
-              @conn.trigger_on_close
-            else
-              send_data_buffered(attempts - 1)
-            end
-          end
-        end
+      def stop
+        return unless @tunnel_path # failed to start
+        req = Net::HTTP::Delete.new(@tunnel_path)
+        request(req, "closing remote tunnel", [200, 202, 204, 404])
+        close
       end
+
+      def request(req, msg, success_codes)
+        req["Auth-Token"] = token
+        retries = 0
+        resp = nil
+
+        loop do
+          retries += 1
+          logger.info("#{msg.capitalize} (retries=#{retries})")
+
+          begin
+            http = Net::HTTP.new(@tun_url.host, @tun_url.port)
+            http.use_ssl = @tun_url.scheme == "https"
+
+            resp = http.request(req)
+
+            break if success_codes.include?(resp.code.to_i)
+
+            logger.debug("Failed #{msg}: HTTP #{resp.code.to_i}")
+          rescue Timeout::Error
+            logger.error("Failed #{msg}: Timeout error")
+          rescue StandardError => e # To satisfy current specs, do we need this really?
+            logger.error("Failed #{msg}: #{e.message}")
+          end
+
+          if retries >= MAX_RETRIES
+            raise ServerError, "Failed #{msg}"
+          end
+        end
+
+        resp
+      end
+
     end
   end
 end

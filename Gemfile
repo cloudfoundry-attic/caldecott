5cd6c87f7880cb3a1184c7020aa76b213902c6f6
Maria Shaldybina <mariash@vmware.com>
2012-11-26 16:00:37 -0800

    Split out client from caldecott
    Change-Id: I5ec315dc824264ddb76774e256709b55f3679ef8

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
 source "http://rubygems.org"
 
 gemspec
-
+gem "caldecott-client", "0.0.1", :git => "https://github.com/cloudfoundry/caldecott-client.git", :ref => "e0111356"
--- lib/caldecott/client.rb
+++
@@ -1,7 +0,0 @@
-# Copyright (c) 2009-2011 VMware, Inc.
-
-require 'caldecott'
-require 'caldecott/client/client'
-require 'caldecott/client/tunnel'
-require 'caldecott/client/http_tunnel'
-require 'caldecott/client/websocket_tunnel'
